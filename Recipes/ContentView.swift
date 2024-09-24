23a14973a978c6a76c5ca930a7d186755dc20ed7
schmru <miha.semrl@gmail.com>
2024-09-24 21:28:15 +0200

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Miha on 24/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Miha on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
